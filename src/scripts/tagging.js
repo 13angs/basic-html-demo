465c43f46d961920d960795edb5e31852b6885df
13angs <13angz.um@gmail.com>
2022-06-26 00:33:21 +0700

    modified last tagging

--- src/scripts/tagging.js
+++ src/scripts/tagging.js
@@ -1,11 +1,21 @@
 // https://stackoverflow.com/questions/29643368/cannot-append-dom-element-to-div-node-uncaught-hierarchyrequesterror-failed-to
-const script = `<script type="text/javascript">
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-})(window, document, "clarity", "script", "bfh9bldt1z");
-</script>
-`;
-const node = document.getElementsByTagName('script')[0];
-const cvtScript = new DOMParser().parseFromString(script, 'text/xml');
-node.parentNode.insertBefore(cvtScript.documentElement, node);
+
+function loadData() {
+  const script = `<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        console.log('success');
+    })(window, document, "clarity", "script", "bfh9bldt1z");
+    </script>
+    `;
+
+  const text = 'hello world';
+
+  // replace the space with empty string
+  console.log(text.replace(/\s/g, ''));
+  const node = document.getElementsByTagName('script')[0];
+  const cvtScript = new DOMParser().parseFromString(script, 'text/xml');
+  node.parentNode.insertBefore(cvtScript.documentElement, node);
+}
+
+window.onload = loadData;
 // document.body.style.background = 'black';
